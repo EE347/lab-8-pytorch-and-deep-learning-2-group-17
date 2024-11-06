f30fc8a8075ddb52eae6084e2711a1da736d5b17
Tadhg Donoghue <t.donoghue2@universityofgalway.ie>
2024-11-06 16:27:43 +0000

    Task 6

--- task6.py
+++ task6.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
         device = torch.device('cpu')
 
     # Hyperparameters
-    learning_rate = 0.001  # Try adjusting this value (e.g., 0.0001, 0.005, 0.01)
-    batch_size = 8  # Try adjusting this value (e.g., 4, 8, 16, 32)
+    learning_rate = 0.005 # Try adjusting this value (e.g., 0.0001, 0.005, 0.01)
+    batch_size = 16  # Try adjusting this value (e.g., 4, 8, 16, 32)
 
     # Create the datasets and dataloaders with modified batch size
     trainset = TeamMateDataset(n_images=50, train=True)
